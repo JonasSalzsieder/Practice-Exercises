26cade4590f056d05b2fe9920312ad170a3636f8
Jonas Salzsieder <jonas.salzsieder@code.berlin>
2025-05-03 17:08:30 +0200

    Exercise 6 done

---
+++ Exercise6.py
@@ -0,0 +1,58 @@
+import random
+
+
+def random_number_within_range(lowest_number, highest_number):
+  return random.randint(lowest_number, highest_number)
+
+def get_user_choice():
+  print("Please select a move. Rock, Paper, or Scissors!")
+  user_choice =input("What do you choose?: ").capitalize()
+  return user_choice
+
+
+def get_computer_choice():  
+    computer_guess = random_number_within_range(1,3)
+    if computer_guess == 1:
+       computer_guess_word = "Rock"
+    elif computer_guess == 2:
+       computer_guess_word = "Paper"
+    else:
+       computer_guess_word = "Scissors"
+    return computer_guess_word
+
+#Game
+
+games_played = 0
+score_player = 0
+score_computer = 0
+score_draw = 0
+
+while games_played <= 5:
+    user_choice = get_user_choice()
+    if user_choice not in ("Rock", "Paper", "Scissors"):
+     user_choice = get_user_choice()
+    else:
+        computer_choice = get_computer_choice()
+        if user_choice == "Rock" and computer_choice == "Scissors":
+            print("Congrats, you won!")
+            score_player += 1
+        elif user_choice == "Paper" and computer_choice == "Rock":
+            print("Congrats, you won!")
+            score_player += 1
+        elif user_choice == "Scissors" and computer_choice == "Paper":
+            print("Congrats, you won!")
+            score_player += 1 
+        elif user_choice == computer_choice:
+            print("Too bad, it's a draw")
+            score_draw += 1
+        else:
+            print("Unfortunate, you lost")
+            score_computer += 1
+        
+        games_played +=1
+
+print("Player Score: " + str(score_player))
+print("Computer Score: " + str(score_computer))
+print("Draw: "+ str(score_draw))
+    
+      
